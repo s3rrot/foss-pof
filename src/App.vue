0b1b72be31c5a46fe40eb8682eaf13d4e26be405
Fer Torres <s3rrot@users.noreply.github.com>
2021-05-05 21:21:28 +0100

    Add description

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div id="app">
+    <h1>💥 FOSS-POF (Free or Open Source Software Points Of Fail)</h1>
+    <p>This is a simple app to self-analyse your Open Source Project and help you take the necessary measures to prevent it from being doomed to fail. <a href="https://github.com/s3rrot/foss-pof">Learn more</a></p>
     <list />
   </div>
 </template>
